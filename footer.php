c8847c131427c4a9eb387b6f250f6d4ac3603814
makrmk <markwerks@gmail.com>
2019-06-20 14:45:24 -0400

    files from live

---
+++ footer.php
@@ -0,0 +1,86 @@
+    <footer class="footer" role="contentinfo">
+    	<div class="grid-container">
+    		<div id="inner-footer" class="grid-x">
+    			<div class="large-4 medium-6 small-12 cell">
+    					<div class="call-to-action-row">
+    						<!-- <div class="small-12 cell">
+    							<label><h4>Sign Up for Our Newsletter</h4></label>
+    						</div> -->
+    						<!-- <div class="small-12 cell">
+    							<button id="footer-button" class="button" value="Sign Up" data-open="footer-contact">Sign Up</button>
+    							<div class="reveal" id="footer-contact" data-reveal>
+    								<iframe src="https://go.pardot.com/l/95432/2017-07-10/2t1kwd" width="100%" height="500" type="text/html" frameborder="0" allowTransparency="true" style="border: 0"></iframe>
+    							</div>
+    						</div> -->
+    						<div id="subsidiaries-row">
+    							<a href="/quienes-somos/pregel-en-el-mundo/" class="button" id="global-button"> 
+    								<div id="globe" class="float-left">
+    									<div id="global-img"></div>
+    								</div>
+    								<div id="global-text">Ver todas las subsidiarias de PreGel</div>
+    							</a>			 
+    						</div>
+    				</div>
+    			</div>
+
+    			<div class="large-4 medium-6 cell">
+    				<ul class="footer-social text-center">
+    					<li>
+    						<a href="https://www.facebook.com/pregelchile/" target="_blank">
+    							<img src="<?php echo get_template_directory_uri(); ?>/assets/images/facebook.png" alt="Facebook" />
+    						</a>
+    					</li>
+    					
+    					<li>
+    						<a href="https://www.linkedin.com/company/pregel-spa" target="_blank">
+    							<img src="<?php echo get_template_directory_uri(); ?>/assets/images/linkedin.png" alt="LinkedIn" />
+    						</a>
+    					</li>
+
+    					<li>
+    						<a href="https://www.youtube.com/user/PregelSPA " target="_blank">
+    							<img src="<?php echo get_template_directory_uri(); ?>/assets/images/youtube.png" alt="YouTube" />
+    						</a>
+    					</li>
+    				</ul>
+    			</div>
+
+    			<div id="right-footer-links" class="large-4 cell">
+    				<ul class="footer-links">
+    					<li class="text-left">
+    						<a href="/contactanos">Contáctenos</a>
+    					</li>
+    					<li class="text-left">
+    						<a href="https://productos.pregelchile.com/pgcl/productos.html">Productos</a>
+    					</li>
+    					<li class="text-left">
+    						<a href="/quienes-somos/acerca-de-pregel">Acerca de PreGel</a>
+    					</li>							
+    				</ul>
+    			</div>
+    		</div> 
+    	</div><!-- end #inner-footer -->
+    </footer> <!-- end .footer -->
+
+    <div id="bottom-footer">
+    	<div id="footer-contact-info">
+    		<div class="grid-container">
+    			<div class="grid-x">
+    				<p class="large-3 medium-4 small-12 text-center cell"><a mailto="info@pregelchile.com">PreGel Chile</a></p>
+    				<p class="large-6 medium-4 small-12 text-center cell">Avda. Ventisqueros 1225, Bodega 40 Work Center Miraflores, Renca.</p>
+    				<p class="large-3 medium-4 small-12 text-center cell">Tel: +56 2 2951 1055</p>
+    				<div class="large-12 medium-12 small-12 text-center cell">
+    					<p id="copyright" class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+    </div>
+</div>  <!-- end .main-content -->
+</div> <!-- end .off-canvas-wrapper-inner -->
+</div> <!-- end .off-canvas-wrapper -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html> <!-- end page -->
